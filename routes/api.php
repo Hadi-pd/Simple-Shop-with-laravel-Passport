edf12f46a9b4a6d691270788d2197a50ecaf808b
Hadi pd <h7ad3i@gmail.com>
2025-04-17 17:48:36 +0330

    Add all

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+
+Route::get('/testpass', function(Request $request){
+    $user = User::find(1);
+    $token = $user->createToken('mobile-app-token')->accessToken;
+    dd($token);
+});
